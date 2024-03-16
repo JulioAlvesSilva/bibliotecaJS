b55f784b33fccb14935a24fd6dd5ed0d61809b4f
Julio Cesar Alves da <138732303+JulioAlvesSilva@users.noreply.github.com>
2024-03-16 11:01:34 -0300

    Update index.tsx

--- src/Componentes/Titulo/index.tsx
+++ src/Componentes/Titulo/index.tsx
@@ -1,31 +1,25 @@
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import styles from './Titulo.module.scss';
 import { MdOutlineSupportAgent } from "react-icons/md";
 import { IoBagHandleOutline } from "react-icons/io5";
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { AppContext } from 'main';
 
 export default function Titulo() {
   const titulos = ["Inicio", "Catálogo", "Sobre nós"];
-  const [labelClicada, setLabelClicada] = useState<number | null>(0);
   const { itensCarrinho } = useContext(AppContext)!;
   const navigate = useNavigate();
-  const handleClick = (index: number) => {
-    setLabelClicada(index);
-    switch (index) {
-      case 0:
-        navigate('/');
-        break;
-      case 1:
-        navigate('/catalogo');
-        break;
-      case 2:
-        navigate('/sobrenos');
-        break;
-      default:
-        break;
-    }
-  };
+  const location = useLocation();
+  let labelClicada: number | null = null;
+
+  if (location.pathname === '/') {
+    labelClicada = 0;
+  } else if (location.pathname === '/catalogo') {
+    labelClicada = 1;
+  } else if (location.pathname === '/sobrenos') {
+    labelClicada = 2;
+  }
+
   return (
     <div>
       <div className={styles.titulo}>
@@ -33,7 +27,7 @@ export default function Titulo() {
         <div className={styles.labels}>
           {titulos.map((item, index) => (
             <label key={index}
-              onClick={() => handleClick(index)}
+            onClick={() => navigate(index === 0 ? '/' : index === 1 ? '/catalogo' : '/sobrenos')}
               style={{ color: labelClicada === index ? 'rgb(105, 105, 105)' : '', textDecoration: labelClicada === index ? 'underline' : '' }}
             >{item}</label>
           ))}
@@ -42,14 +36,13 @@ export default function Titulo() {
           <div className={styles.icons__sacola}
             onClick={() => {
               navigate("/sacola");
-              handleClick(5)
             }} >
             <div className={styles.icons__sacola__clique}>
               <IoBagHandleOutline size={30} style={{ marginRight: '20px' }} />
               <label style={{display: itensCarrinho.length !== 0 ? 'flex' : 'none' }}>{itensCarrinho.length}</label>
             </div>
           </div>
-          <MdOutlineSupportAgent size={30} color='rgb(1, 1, 85)' className={styles.icons__svg2} />
+          <MdOutlineSupportAgent size={30} color='rgb(1, 1, 85)' className={styles.icons__svg2}/>
         </div>
       </div>
     </div>
