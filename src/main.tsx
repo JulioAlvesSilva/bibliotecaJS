8083369d357ea8b0ca3194e9038159df8a0e8d64
Julio Cesar Alves da <138732303+JulioAlvesSilva@users.noreply.github.com>
2024-03-16 17:15:26 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,10 @@ interface AppContextType {
     filtro: number | null;
     setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
     SelecFiltro: (item: Ifiltro) => void;
+    CliqueNoFace: () => void;
+    CliqueWhats: () => void;
+    CliqueNoIsta: () => void;
+    CliqueNoLinkedin: () => void;
 }
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -17,14 +21,36 @@ export const AppProvider = ({ children }: { children: ReactNode }) => {
 
     const [itensCarrinho, setItensCarrinho] = useState<Iitem[]>([]);
     const [filtro, setFiltro] = useState<number | null>(null);
+    const phoneNumber = '62993129673';
+    const message = 'Olá, como posso ajudar?';
 
     function SelecFiltro(item: Ifiltro) {
-        if(filtro === item.id) return setFiltro(null)
-         return setFiltro(item.id)
+        if (filtro === item.id) return setFiltro(null)
+        return setFiltro(item.id)
+    }
+
+    const CliqueWhats = () => {
+        const isMobile = window.innerWidth <= 700;
+        const baseUrl = isMobile ? `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`
+            : `https://web.whatsapp.com/send?phone=${encodeURIComponent(phoneNumber)}&text=${encodeURIComponent(message)}`
+        window.open(baseUrl, '_blank');
+    };
+    const CliqueNoIsta = () => {
+        const instagram = "https://www.instagram.com/jswebprogramador?igsh=MWl5dnRqdHg1bjM2OQ=="
+        window.open(instagram, '_blank');
+    }
+    const CliqueNoFace = () => {
+        const facebook = "https://www.facebook.com/profile.php?id=61553007513798&mibextid=ZbWKwL"
+        window.open(facebook, '_blank');
+    }
+
+    const CliqueNoLinkedin = () => {
+        const linkedin = "www.linkedin.com/in/júlio-cesar-alves-da-silva-b72732a8"
+        window.open(linkedin, '_blank');
     }
 
     return (
-        <AppContext.Provider value={{ setItensCarrinho, itensCarrinho, SelecFiltro, setFiltro, filtro  }}>
+        <AppContext.Provider value={{ setItensCarrinho, itensCarrinho, SelecFiltro, setFiltro, filtro, CliqueNoFace, CliqueNoIsta, CliqueWhats, CliqueNoLinkedin }}>
             {children}
         </AppContext.Provider>
     );
